69c34375d04af991c1857ef2a7a6c1fb4e260868
Matt Rhone <kritoke@users.noreply.github.com>
2020-07-04 06:22:01 -0500

    finish exercise 2.

--- the_basics/exercise2.js
+++ the_basics/exercise2.js
@@ -1,7 +1,9 @@
-const NUMBER = 4936;
+let number = 4936;
 
-let onesPlace = NUMBER % 10;
-let tensPlace = ((NUMBER - onesPlace) / 10) % 10;
-let hundredsPlace = ((NUMBER - onesPlace - tensPlace) / 10) % 10;
-
-console.log(hundredsPlace);
+let onesPlace = number % 10;
+number = (number - onesPlace) / 10;
+let tensPlace = number % 10;
+number = (number - tensPlace) / 10; 
+let hundredsPlace = number % 10;
+number = (number - hundredsPlace) / 10;
+let thousandsPlace = number % 10;
